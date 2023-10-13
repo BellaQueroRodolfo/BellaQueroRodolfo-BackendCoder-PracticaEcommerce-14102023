173ce34f00bb02f510e2989ae190a9e3cbf83be8
BellaQueroRodolfo <83620265+BellaQueroRodolfo@users.noreply.github.com>
2023-10-13 20:14:55 -0300

    Update index.js

--- BellaQueroRodolfo-BackendCoder-PracticaEcommerce-14102023/src/dao/index.js
+++ BellaQueroRodolfo-BackendCoder-PracticaEcommerce-14102023/src/dao/index.js
@@ -1,1 +1,16 @@
+const mongoose = require('mongoose');
 
+mongoose.connect('mongodb://localhost:27017/ecommerce', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+
+const db = mongoose.connection;
+
+db.on('error', (error) => {
+  console.error('MongoDB connection error:', error);
+});
+
+db.once('open', () => {
+  console.log('Connected to MongoDB');
+});

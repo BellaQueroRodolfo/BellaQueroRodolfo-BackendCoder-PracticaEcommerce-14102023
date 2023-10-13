5e8105478a179b57d34617687c4717c028b0f08f
BellaQueroRodolfo <83620265+BellaQueroRodolfo@users.noreply.github.com>
2023-10-13 20:16:05 -0300

    Update app.js

--- BellaQueroRodolfo-BackendCoder-PracticaEcommerce-14102023/src/app.js
+++ BellaQueroRodolfo-BackendCoder-PracticaEcommerce-14102023/src/app.js
@@ -2,6 +2,7 @@ const express = require('express');
 const exphbs = require('express-handlebars');
 const http = require('http');
 const socketIo = require('socket.io');
+const mongoose = require('mongoose');
 const app = express();
 const server = http.createServer(app);
 const io = socketIo(server);
@@ -12,19 +13,40 @@ app.use(express.json());
 const productsRouter = require('./routes/product');
 const cartsRouter = require('./routes/cart');
 
+
 app.engine('handlebars', exphbs.engine());
 app.set('view engine', 'handlebars');
 app.use(express.static('public'));
 app.use('/products', productsRouter);
 app.use('/carts', cartsRouter);
+
 app.get('/real-time-products', (req, res) => {
   res.render('realTimeProducts', { layout: 'main' });
+});
+
+app.get('/chat', (req, res) => {
+  res.render('chat', { layout: 'main' });
 });
 
 io.on('connection', (socket) => {
   socket.on('new-product', (product) => {
     io.emit('product-added', product);
   });
+});
+
+mongoose.connect('mongodb://localhost:27017/ecommerce', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+
+const db = mongoose.connection;
+
+db.on('error', (error) => {
+  console.error('MongoDB connection error:', error);
+});
+
+db.once('open', () => {
+  console.log('Connected to MongoDB');
 });
 
 server.listen(port, () => {
